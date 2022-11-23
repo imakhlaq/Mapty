f8e32379734923373cfe9a968d570f2ac6c74aee
imakhlaq <imakhlaqXD@gmail.com>
2022-11-23 09:13:09 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+'use strict';
+
+// prettier-ignore
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+
+//getting user location thorugh browser API
+
+navigator.geolocation?.getCurrentPosition(
+  function (p) {
+    //taking latitude and longitude out of p
+    const { latitude, longitude } = p.coords;
+
+    //making a url based on longitude and latitude
+    // const locationURL = `https://www.google.com/maps/search/?api=1&query=${latitude},${longitude}`;
+
+    //from leaflet API
+
+    const map = L.map('map').setView([latitude, longitude], 16);
+
+    L.tileLayer(
+      'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+      {
+        attribution:
+          'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
+      }
+    ).addTo(map);
+
+    L.marker([latitude, longitude])
+      .addTo(map)
+      .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+      .openPopup();
+  },
+  function () {
+    prompt('Please Allow location acces');
+  }
+);
+
