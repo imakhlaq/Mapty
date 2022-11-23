3c1f270928209678f8abaf809dd578b015bf09cf
imakhlaq <imakhlaqXD@gmail.com>
2022-11-23 15:54:56 +0530

    update

--- script.js
+++ script.js
@@ -23,21 +23,48 @@ navigator.geolocation?.getCurrentPosition(
 
     //from leaflet API
 
-    const map = L.map('map').setView([latitude, longitude], 14);
+    const map = L.map('map').setView([latitude, longitude], 20);
 
     L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
       maxZoom: 18,
       subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
     }).addTo(map);
+    //marker on click on map
+    //this map object contain a on method which is like a addeventlistner and when you click on it also passed and event object to the function
+    map.on('click', function (event) {
+      console.log(event);
 
-    L.marker([latitude, longitude])
-      .addTo(map)
-      .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-      .openPopup();
+      //showing form
+      form.classList.remove('hidden');
+
+      const showMarker = function (e) {
+        e.preventDefault();
+        if (e.key === 'Enter') {
+          //taking out lat and lang from event
+          const { lat, lng } = event.latlng;
+          console.log(lat, lng);
+          //adding marker on clicked location
+          L.marker([lat, lng])
+            .addTo(map)
+            .bindPopup(
+              L.popup({
+                maxWidth: 250,
+                minWidth: 100,
+                autoClose: false,
+                closeOnClick: false,
+                className: 'running-popup',
+              })
+            )
+            .setPopupContent('jio')
+            .openPopup();
+        }
+      };
+
+      //and showing maker on clikced
+      form.addEventListener('keypress', showMarker);
+    });
   },
   function () {
     prompt('Please Allow location acces');
   }
 );
-
-
